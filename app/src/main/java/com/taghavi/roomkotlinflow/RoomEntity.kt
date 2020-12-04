fa94211ffab052326e0dcefdf93bdc61d572b8bc
EhsanTgv <EhsanTgv@gmail.com>
2020-12-04 13:08:26 +0330

    add FavTvShowsDao interface

--- app/src/main/java/com/taghavi/roomkotlinflow/RoomEntity.kt
+++ app/src/main/java/com/taghavi/roomkotlinflow/RoomEntity.kt
@@ -1,10 +1,21 @@
 package com.taghavi.roomkotlinflow
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.lifecycle.LiveData
+import androidx.room.*
 
 @Entity
 data class FavTVShows(@PrimaryKey val name: String,
                       val rating : Int,
                       val finished : Boolean) {
+}
+
+@Dao
+interface FavTvShowsDao {
+
+    @Query("SELECT * FROM FavTvShows")
+    fun getAllShows(): LiveData<List<FavTVShows>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun setAllShows(shows : List<FavTVShows>)
+
 }
